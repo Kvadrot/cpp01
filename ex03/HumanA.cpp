821bab286c5f94798c702ada445611b24d2bc3c1
Kvadrot <send.me.your.maild@gmail.com>
2025-03-26 11:20:29 +0100

    HumanA is improved: deleted unnecessary assignment

--- ex03/HumanA.cpp
+++ ex03/HumanA.cpp
@@ -3,29 +3,25 @@
 /*                                                        :::      ::::::::   */
 /*   HumanA.cpp                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ufo <ufo@student.42.fr>                    +#+  +:+       +#+        */
+/*   By: itykhono <itykhono@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/21 21:07:58 by ufo               #+#    #+#             */
-/*   Updated: 2025/01/22 18:14:15 by ufo              ###   ########.fr       */
+/*   Updated: 2025/03/26 11:18:50 by itykhono         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "HumanA.hpp"
 // Constructors:
-HumanA::HumanA(std::string name, Weapon &weapon) : _weapon(weapon), _name (name)
-{
-    this->_name = name;
-    this->_weapon = weapon;
+HumanA::HumanA(std::string name, Weapon &weapon) : _weapon(weapon), _name (name) {
+
 }
 
-HumanA::~HumanA()
-{
+HumanA::~HumanA() {
     
 }
 
 // Methods
-void HumanA::attack( void )
-{
+void HumanA::attack( void ) {
     std::cout << _name << " attacks with their " << _weapon.getType() << std::endl;
 }
 
